2cbd843b622a1710ae252e934b1b832667f89a84
boss <jeltonogov@mail.ru>
2021-04-15 09:40:16 +0400

    Реализована домашняя страница на основе шаблона base.html. ФТ и МТ - ОК

--- ft/test_dashboard.py
+++ ft/test_dashboard.py
@@ -5,13 +5,15 @@ from ft.base import FunctionalTest
 
 class DashboardTest(FunctionalTest):
 
-    def test_can_start_dashboard(self):
+    def test_can_start_dashboard_and_styling(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        self.assertIn('Обменный пункт', self.browser.title)
+        navbar = self.browser.find_element_by_class_name('navbar-brand').text
+        self.assertIn('Exchange', navbar)
+        sidebar = self.browser.find_element_by_class_name('sidebar-sticky').text
+        self.assertIn('Покупка', sidebar)
 
